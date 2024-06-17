0c65c6e02d2d8e8213401e1f327b86fa84c4739e
Amir Raminfar <findamir@gmail.com>
2024-06-17 16:02:36 -0700

    updates mods

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import "@unocss/reset/tailwind-compat.css";
 import "virtual:uno.css";
 import "./custom.css";
 import { ViteSSG } from "vite-ssg";
+import { ref } from "vue";
 import App from "./App.vue";
 import { routes } from "./router";
 
-export const createApp = ViteSSG(App, { routes }, ({ app, router, routes, isClient, initialState }) => {});
+export const data = ref({ starts: 0 });
+export const createApp = ViteSSG(App, { routes }, async ({ app, router, routes, isClient, initialState }) => {});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import vuePlugin from "@vitejs/plugin-vue";
-import svgLoader from "vite-svg-loader";
+import Vue from "@vitejs/plugin-vue";
+import SVGLoader from "vite-svg-loader";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
   css: {
     transformer: "lightningcss",
   },
-  plugins: [UnoCSS(), vuePlugin(), svgLoader()],
+  plugins: [UnoCSS(), Vue(), SVGLoader()],
 });
